d2e70ffc74e46da0951570f044ee7987e5535472
unknown <mulkyway00@gmail.com>
2023-05-31 00:18:20 +0300

    calculator ver 2.0

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -4,7 +4,7 @@ import { Form, Container, Row, Col } from "react-bootstrap";
 
 const Calculator = () => {
   const [websiteType, setWebsiteType] = useState("");
-  const [websitePages, setWebsitePages] = useState(1);
+  const [websitePages, setWebsitePages] = useState(0);
   const [websiteFeatures, setWebsiteFeatures] = useState("");
   const [contactForm, setContactForm] = useState(false);
   const [feedbackForm, setFeedbackForm] = useState(false);
@@ -19,8 +19,14 @@ const Calculator = () => {
     if (selectedType === "landing") {
       setWebsitePages(1);
       setSliderProgress(0);
-    } else {
-      setSliderProgress((websitePages / 10) * 100);
+    } else if (selectedType === "multi") {
+      setWebsitePages(1);
+      setSliderProgress(10);
+      if (designType === "custom") {
+        setEstimatedCost(5000);
+      } else {
+        setEstimatedCost(0);
+      }
     }
 
     setWebsiteType(selectedType);
@@ -50,11 +56,16 @@ const Calculator = () => {
 
   const handleDesignTypeChange = (event) => {
     setDesignType(event.target.value);
+    if (websiteType === "multi" && event.target.value === "custom") {
+      setEstimatedCost(5000);
+    } else {
+      setEstimatedCost(0);
+    }
   };
 
   const calculateCost = () => {
     const typeCost = websiteType === "landing" ? 5000 : 0;
-    const pagesCost = websitePages * 1500;
+    const pagesCost = websitePages * 3000;
     const featuresCost = websiteFeatures * 1000;
     const contactFormCost = contactForm ? 2000 : 0;
     const feedbackFormCost = feedbackForm ? 1500 : 0;
@@ -78,85 +89,92 @@ const Calculator = () => {
   return (
     <>
       <Container>
-      <Row className="justify-content-center">
-        <Col md={6}>
-          <div className="calculator">
-            <h2>Калькулятор</h2>
-            <Form className="form-calculator">
-              <Form.Group>
-                <Form.Label>Тип веб-сайта:</Form.Label>
-                <Form.Control as="select" value={websiteType} onChange={handleWebsiteTypeChange}>
-                  <option value="">Выберите тип</option>
-                  <option value="landing">Одностраничный веб-сайт</option>
-                  <option value="multi">Многостроничный веб-сайт</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Количество страниц:</Form.Label>
-                <div className="slider-container">
-                  <Form.Range min={1} max={10} value={websitePages} onChange={handleWebsitePagesChange} disabled={websiteType === "landing"} />
-                  <div className="slider-progress" style={{ width: `${sliderProgress}%` }} />
-                </div>
-                <p>{websitePages}</p>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Тип дизайна:</Form.Label>
-                <div className="design-type">
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <div className="calculator">
+              <h2>Калькулятор</h2>
+              <Form className="form-calculator">
+                <Form.Group>
+                  <Form.Label>Тип веб-сайта:</Form.Label>
+                  <Form.Control as="select" value={websiteType} onChange={handleWebsiteTypeChange}>
+                    <option value="">Выберите тип</option>
+                    <option value="landing">Одностраничный веб-сайт</option>
+                    <option value="multi">Многостроничный веб-сайт</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Количество страниц:</Form.Label>
+                  <div className="slider-container">
+                    <Form.Range
+                      min={0}
+                      max={10}
+                      value={websitePages}
+                      onChange={handleWebsitePagesChange}
+                      disabled={websiteType === "landing"}
+                    />
+                    <div className="slider-progress" style={{ width: `${sliderProgress}%` }} />
+                  </div>
+                  <p>{websitePages}</p>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Тип дизайна:</Form.Label>
+                  <div className="design-type">
+                    <Form.Check
+                      type="radio"
+                      label="Авторский дизайн"
+                      name="designType"
+                      value="custom"
+                      checked={designType === "custom"}
+                      onChange={handleDesignTypeChange}
+                    />
+                    <Form.Check
+                      type="radio"
+                      label="Шаблонный дизайн"
+                      name="designType"
+                      value="template"
+                      checked={designType === "template"}
+                      onChange={handleDesignTypeChange}
+                    />
+                  </div>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Дополнительные функции:</Form.Label>
                   <Form.Check
-                    type="radio"
-                    label="Авторский дизайн"
-                    name="designType"
-                    value="custom"
-                    checked={designType === "custom"}
-                    onChange={handleDesignTypeChange}
+                    type="checkbox"
+                    label="Контактная форма"
+                    checked={contactForm}
+                    onChange={handleContactFormChange}
                   />
                   <Form.Check
-                    type="radio"
-                    label="Шаблонный дизайн"
-                    name="designType"
-                    value="template"
-                    checked={designType === "template"}
-                    onChange={handleDesignTypeChange}
+                    type="checkbox"
+                    label="Форма обратной связи"
+                    checked={feedbackForm}
+                    onChange={handleFeedbackFormChange}
                   />
-                </div>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Дополнительные функции:</Form.Label>
-                <Form.Check
-                  type="checkbox"
-                  label="Контактная форма"
-                  checked={contactForm}
-                  onChange={handleContactFormChange}
-                />
-                <Form.Check
-                  type="checkbox"
-                  label="Форма обратной связи"
-                  checked={feedbackForm}
-                  onChange={handleFeedbackFormChange}
-                />
-                <Form.Check
-                  type="checkbox"
-                  label="Онлайн-чат"
-                  checked={onlineChat}
-                  onChange={handleOnlineChatChange}
-                />
-              </Form.Group>
-            </Form>
-            {estimatedCost > 0 && (
-              <motion.div
-                className="result"
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                <h3>Примерная стоимость:</h3>
-                <p>{estimatedCost} ℗</p>
-              </motion.div>
-            )}
-          </div>
-        </Col>
-      </Row>
-    </Container>
+                  <Form.Check
+                    type="checkbox"
+                    label="Онлайн-чат"
+                    checked={onlineChat}
+                    onChange={handleOnlineChatChange}
+                  />
+                </Form.Group>
+              </Form>
+              {estimatedCost > 0 && (
+                <motion.div
+                  className="result"
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <h3>Примерная стоимость:</h3>
+                  <p>{estimatedCost} ℗</p>
+                  <p className="calculator-warning">*указанная стоимость является приблизительной и может изменяться в зависимости от сложности работы.</p>
+                </motion.div>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
